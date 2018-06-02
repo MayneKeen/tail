d66e7a4579ea059e3ecacb9781ed7cf0e9b533fb
MayneKeen <menpet2000@gmail.com>
2018-06-03 02:44:01 +0300

    done

--- src/com/keenpro/Func.java
+++ src/com/keenpro/Func.java
@@ -4,21 +4,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Func {
 
     public static PrintStream out = System.out;
 
-    public static ArrayList<String> action(ArrayList<String> input, ArrayList<String> inputFiles,
-                                           int c, int n) {
+    public static LinkedList<String> action(ArrayList<String> input, ArrayList<String> inputFiles,
+                                            int c, int n, boolean consoleInput) {
 
         try {
 
             String fileName;
-            ArrayList<String> result = new ArrayList<>();
+            LinkedList<String> result = new LinkedList<>();
 
             for (String element : inputFiles) {
+
+                out.println("Filename is " + element);
 
                 fileName = element;
                 FileReader fr = new FileReader(fileName);         // tail [-c num |-n num] [-o file] file0 file1 file2...
@@ -34,11 +37,36 @@ public class Func {
                         result.add("\n");
                         result.add(list.get(k));
                     }
-                } else {
+                }
+
+
+                /*else {
+                    int l = list.size()-1;
+                    int a = 0;
+                    int k = 0;
+                    int i = l;
+
+                    while (c>k) {
+                        i--;
+                        a++;
+                        c-=list.get(i).length();
+                    }
+
+                    while (c>list.get(k).length();) {
+
+
+                        c-=list.get(k).length();
+                    }
+
+
+                }*/
+
+
+                else {
                     int k = list.size() - 1;
                     while (c < list.get(k).length()) {
-                        result.add(list.get(k));
-                        result.add("\n");
+                        result.addFirst(list.get(k));
+                        result.addFirst("\n");
                         c -= list.get(k).length();
                         k++;
                     }
@@ -46,19 +74,49 @@ public class Func {
                     String last = list.get(k);
                     if (c > 0) {
                         for (int l = last.length() - 1 - c; l < last.length(); l++) {
-                            result.add(Character.toString(last.charAt(l)));
+                            result.addFirst(Character.toString(last.charAt(l)));
                         }
                     }
                 }
                 fr.close();
             }
             return result;
-        }
-        catch (IOException e) {
+
+
+        } catch (IOException e) {
             out.println("IOException has just been caught (FileReader)");
             return null;
         }
     }
 
+
+    public static LinkedList<String> consoleAction(LinkedList<String> input,
+                                                   int c, int n) {
+
+        LinkedList<String> result = new LinkedList<>();
+
+        if (c == 0) {
+            for (int k = input.size() - 1 - n; k < (input.size() - 1); k++) {
+                input.add("\n");
+                result.add(input.get(k));
+            }
+        } else {
+            int k = input.size() - 1;
+            while (c < input.get(k).length()) {
+                result.addFirst(input.get(k));
+                result.addFirst("\n");
+                c -= input.get(k).length();
+                k++;
+            }
+
+            String last = input.get(k);
+            if (c > 0) {
+                for (int l = last.length() - 1 - c; l < last.length(); l++) {
+                    result.addFirst(Character.toString(last.charAt(l)));
+                }
+            }
+        }
+        return result;
+    }
 
 }
--- src/com/keenpro/Tail.java
+++ src/com/keenpro/Tail.java
@@ -1,15 +1,13 @@
 package com.keenpro;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-
 import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class Tail  {
-    //public static Scanner in = new Scanner(System.in);
+public class Tail {
+    public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
 
 
@@ -26,11 +24,12 @@ public class Tail  {
                 input.add(args[i]);
                 Matcher m = p.matcher(args[i]);
                 if (!m.matches() && !args[i].equals("tail") && !args.equals("-c") &&
-                        !args.equals("-n") && !args.equals("-o") && args[i-1]!="-o")
+                        !args.equals("-n") && !args.equals("-o") && args[i - 1] != "-o")
                     inputFiles.add(args[i]);
             }
 
-
+            int c = 0;
+            int n = 0;
             String ofile = "";
             if (input.contains("-o")) {
                 ofile = input.get(input.indexOf("-o") + 1);
@@ -39,44 +38,63 @@ public class Tail  {
             if (input.contains("-c") && input.contains("-n")) {
                 out.print("-c and -n flags are used together");
             } else {
-                int c = 0;
-                int n = 0;
+
                 if (input.contains("-c")) {
                     c = Integer.parseInt(input.get(input.indexOf("-c") + 1));
                 } else if (input.contains("-n")) {
                     n = Integer.parseInt(input.get(input.indexOf("-n") + 1));
                 }
+            }
 
+            if (!inputFiles.isEmpty()) {
                 if (ofile != "") {
                     FileWriter fw = new FileWriter(ofile);
-                    ArrayList<String> result = Func.action(input, inputFiles, c, n);
-                    if (result==null)
-                        return;
-                    for(int i=0; i<result.size(); i++) {
-                        fw.write(result.toString());
+                    LinkedList<String> result = Func.action(input, inputFiles, c, n, false);
+                    for (int i = 0; i < result.size(); i++) {
+                        fw.write(result.get(i) + " ");
                     }
                     fw.close();
 
                 } else {
-                    ArrayList<String> result = Func.action(input, inputFiles, c, n);
-                    if (result==null)
-                        return;
-                    for(int i=0; i<result.size(); i++) {
-                        out.println(result.toString());
+                    LinkedList<String> result = Func.action(input, inputFiles, c, n, false);
+                    for (int i = 0; i < result.size(); i++) {
+                        out.println(result.get(i) + " ");
                     }
 
                 }
 
+            } else {
+
+                LinkedList<String> text = new LinkedList<>();
+                out.println("Please, enter your text here, after it please write \"-s\" in a new line");
+                String temp = in.nextLine();
+                while (temp != "-s") {
+                    text.addLast(temp);
+                    temp = in.nextLine();
+                }
+
+                if (ofile != "") {
+                    FileWriter fw = new FileWriter(ofile);
+                    LinkedList<String> result = Func.consoleAction(text, c, n);
+                    for (int i = 0; i < result.size(); i++) {
+                        fw.write(result.get(i) + " ");
+                    }
+                    fw.close();
+
+                } else {
+                    LinkedList<String> result = Func.consoleAction(text, c, n);
+                    for (int i = 0; i < result.size(); i++) {
+                        out.println(result.get(i) + " ");
+                    }
+
+                }
             }
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             out.println("IOException has just been caught (FileWriter)");
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             out.println("The action() method did smth wrong");
         }
+
+
     }
-
-
-
 }
